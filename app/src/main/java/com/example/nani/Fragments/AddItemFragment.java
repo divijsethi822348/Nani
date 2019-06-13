c1a91b9ae2b1b028f904818d10c2477a53b2be7f
Divij <divij.edcellpup@gmail.com>
2019-06-13 13:04:41 +0530

    init

--- app/src/main/java/com/example/nani/Fragments/AddItemFragment.java
+++ app/src/main/java/com/example/nani/Fragments/AddItemFragment.java
@@ -8,6 +8,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
 
 import com.example.nani.Adapters.AddItemRecyclerAdapter;
 import com.example.nani.R;
@@ -16,6 +18,8 @@ import com.example.nani.R;
  * A simple {@link Fragment} subclass.
  */
 public class AddItemFragment extends Fragment {
+    CheckBox contains_meat;
+    LinearLayout meat_products;
     RecyclerView addItemRecycler;
 
 
@@ -29,6 +33,19 @@ public class AddItemFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view=inflater.inflate(R.layout.fragment_add_item, container, false);
+        contains_meat= view.findViewById(R.id.contains_meat_products_check);
+        meat_products=view.findViewById(R.id.contains_meat_products_layout);
+        contains_meat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (contains_meat.isChecked()){
+                    meat_products.setVisibility(View.VISIBLE);
+                }else {
+                    meat_products.setVisibility(View.GONE);
+                }
+            }
+        });
+
         addItemRecycler=view.findViewById(R.id.add_item_recycler_view);
         addItemRecycler.setLayoutManager(new LinearLayoutManager(getContext(),LinearLayoutManager.HORIZONTAL,false));
         addItemRecycler.setAdapter(new AddItemRecyclerAdapter(getContext()));
